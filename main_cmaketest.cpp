5e5fd9b7bf6be4f5f988a677201f0d5da1eff2a2
Kishan Patel <imkishan96@gmail.com>
2020-03-29 00:35:17 -0500

    Cmake update | addede PCL support | added PCL visualization

--- main_cmaketest.cpp
+++ main_cmaketest.cpp
@@ -1,22 +1,65 @@
 #include<iostream>
 #include<librealsense2/rs.hpp>
+#include<pcl/point_cloud.h>
+#include<pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+using pcl_ptr = pcl::PointCloud<pcl::PointXYZ>::Ptr;
+
+pcl_ptr points_to_pcl(const rs2::points& points)
+{
+    pcl_ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    auto sp = points.get_profile().as<rs2::video_stream_profile>();
+    cloud->width = sp.width();
+    cloud->height = sp.height();
+    cloud->is_dense = false;
+    cloud->points.resize(points.size());
+    auto ptr = points.get_vertices();
+    for (auto& p : cloud->points)
+    {
+        p.x = ptr->x;
+        p.y = ptr->y;
+        p.z = ptr->z;
+        ptr++;
+    }
+
+    return cloud;
+}
 
 int main(){
     std::cout << "Cmake workes and exe created" << std::endl;
     
     rs2::pipeline p;
+    rs2::frameset frames;
+    rs2::frame fff;
+    auto depth = fff.as<rs2::depth_frame>();
+    rs2::pointcloud pc;
+    rs2::points points;
+    pcl::visualization::CloudViewer viewer("Cloud Viewer");
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_points;
+    rs2::config cfg;
 
-    // Configure and start the pipeline
-    p.start();
+    cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
+    cfg.enable_stream(RS2_STREAM_DEPTH, RS2_FORMAT_ANY);
+    p.start(cfg);
+    frames = p.wait_for_frames();
+    depth = frames.get_depth_frame();        
+    points = pc.calculate(depth);
+    pcl_points = points_to_pcl(points);
 
-    while (true)
+    viewer.showCloud(pcl_points);
+
+    while (!viewer.wasStopped ())
     {
         // Block program until frames arrive
-        rs2::frameset frames = p.wait_for_frames();
-
+        frames = p.wait_for_frames();
         // Try to get a frame of a depth image
-        rs2::depth_frame depth = frames.get_depth_frame();
-
+        depth = frames.get_depth_frame();        
+        points = pc.calculate(depth);
+        pcl_points = points_to_pcl(points);
+        
         // Get the depth frame's dimensions
         float width = depth.get_width();
         float height = depth.get_height();
